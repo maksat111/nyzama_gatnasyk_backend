4254f977bce48a8511c6a01c0dc85e36aa9b53b6
maksat111 <maksatakmyradow111@gmail.com>
2023-01-20 03:29:44 +0500

    added get brakings

--- controllers/breakingRulesController.js
+++ controllers/breakingRulesController.js
@@ -18,4 +18,27 @@ const createBreaking = async (req, res) => {
     }
 }
 
+const getBreaking = async (req, res) => {
+    try {
+        const { created_at, type } = req.query;
+        let foundBreakings = [];
+
+        if (type == 'late') {
+            foundBreakings = await BreakingRules.find({ created_at, late: true });
+        }
+
+        if (type == 'uniform') {
+            foundBreakings = await BreakingRules.find({ created_at, uniform: true });
+        }
+
+        res.status(200).json({ success: 1, data: foundBreakings });
+    } catch (err) {
+        res.status(500).json({
+            success: 0,
+            msg: err.message
+        })
+    }
+}
+
 exports.createBreaking = createBreaking;
+exports.getBreaking = getBreaking;
--- router/routes.js
+++ router/routes.js
@@ -8,32 +8,33 @@ const teacherController = require('../controllers/teachersController');
 const groupsController = require('../controllers/groupsController');
 const breakingRules = require('../controllers/breakingRulesController');
 
-//--------------------------------------------- Middlewares --------------------------------------------- //
+//--------------------------------------------- Middlewares ------------------------------------------------------- //
 const auth = require('../middlewares/auth');
 
-// -------------------------------------------- Authentication Routes ----------------------------------------- //
+// -------------------------------------------- Authentication Routes --------------------------------------------- //
 router.post('/auth/login', authController.teacherLogin);
 
-//---------------------------------------------- Teachers Routes ---------------------------------------------- //
+//---------------------------------------------- Teachers Routes -------------------------------------------------- //
 router.post('/teacher/create', auth, teacherController.createTeacher);
 router.get('/teacher', auth, teacherController.getAllTeachers);
 router.delete('/teacher/delete/:teacher_id', auth, teacherController.deleteTeacher);
 router.patch('/teacher/update/:teacher_id', auth, teacherController.updateTeacher);
 
-//---------------------------------------------- Groups Routes ---------------------------------------------- //
+//---------------------------------------------- Groups Routes ---------------------------------------------------- //
 router.get('/groups', auth, groupsController.getAllGroups);
 router.post('/groups/create', auth, groupsController.createGroup);
 router.delete('/groups/delete/:group_id', auth, groupsController.deleteGroup);
 router.patch('/groups/update/:group_id', auth, groupsController.updateGroup);
 router.get('/groups/search', auth, groupsController.searchGroup);
 
-//---------------------------------------------- Students Routes ---------------------------------------------- //
+//---------------------------------------------- Students Routes --------------------------------------------------- //
 router.get('/students/:group_id', auth, studentController.getStudentsByGroupId);
 router.post('/students/create', auth, studentController.createStudent);
 router.delete('/students/delete/:student_id', auth, studentController.deleteStudent);
 router.patch('/students/update/:student_id', auth, studentController.updateStudent);
 
-//---------------------------------------------- Students Routes ---------------------------------------------- //
+//---------------------------------------------- Rule Breaking Routes ---------------------------------------------- //
 router.post('/breaking/create', auth, breakingRules.createBreaking);
+router.get('/breaking', auth, breakingRules.getBreaking);
 
 module.exports = router;
