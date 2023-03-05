fc123cd072c48e35a5729fcee35e01fcb6d3d52c
maksat111 <maksatakmyradow111@gmail.com>
2023-03-06 03:04:51 +0500

    With hope everything is OK

--- controllers/breakingRulesController.js
+++ controllers/breakingRulesController.js
@@ -1,19 +1,23 @@
 const BreakingRules = require('../models/breakingRules');
+const Students = require('../models/students');
 const date = require('date-and-time');
 
 const createBreaking = async (req, res) => {
     try {
         const breakings = req.body;
 
-        breakings.forEach(element => {
-            element.created_at = date.format(new Date(), 'YYYY-MM-DD');
+        breakings.forEach(async element => {
+            if (!element.created_at) {
+                element.created_at = date.format(new Date(), 'YYYY-MM-DD');
+            }
+            const added = await Students.update(
+                { _id: element.student_id },
+                { $push: { breakingRules: { created_at: element.created_at, late: element.late, uniform: element.uniform, note: element.note } } }
+            );
         });
-
-        const createdBreakings = await BreakingRules.insertMany(breakings);
 
         res.status(201).json({
             success: 1,
-            createdBreakings
         })
     } catch (err) {
         res.status(500).json({
@@ -27,9 +31,9 @@ const getBreaking = async (req, res) => {
     try {
         const { created_at, group_id } = req.query;
 
-        const founded = await BreakingRules.find({ created_at, group_id }).populate('student_id');
+        const founded = await Students.find({ group_id }).populate('group_id');
         // console.log(date.format(new Date(), 'YYYY/MM/DD HH:mm:ss'));
-        res.send(founded)
+        res.send({ success: 1, data: founded })
     } catch (err) {
         res.status(500).json({
             success: 0,
